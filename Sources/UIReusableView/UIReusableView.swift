ad2459d7a3904c6fc866501717de87bd6b9a08e9
Zach Burky <zach.burky@willowtreeapps.com>
2021-07-19 09:04:48 -0500

    Adds initial source for UICollectionViewCell and UITableViewCell

--- Sources/UIReusableView/UIReusableView.swift
+++ Sources/UIReusableView/UIReusableView.swift
@@ -1,3 +1,37 @@
-struct UIReusableView {
-    var text = "Hello, World!"
+import UIKit
+
+public protocol UIReusableView: UIView {
+    static var identifier: String { get }
+}
+
+public extension UIReusableView {
+    static var identifier: String { .init(describing: self) }
+}
+
+public extension UIReusableView where Self: UICollectionViewCell {
+    static func load<T: UIReusableView>() -> T {
+        Bundle.main.loadNibNamed(identifier, owner: nil)!.first as! T
+    }
+
+    static func dequeue<T: UIReusableView>(from collectionView: UICollectionView, at indexPath: IndexPath) -> T {
+        collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! T
+    }
+
+    static func register(in collectionView: UICollectionView) {
+        collectionView.register(UINib(nibName: identifier, bundle: nil), forCellWithReuseIdentifier: identifier)
+    }
+}
+
+public extension UIReusableView where Self: UITableViewCell {
+    static func load<T: UIReusableView>() -> T {
+        Bundle.main.loadNibNamed(identifier, owner: nil)!.first as! T
+    }
+
+    static func dequeue<T: UIReusableView>(from tableView: UITableView, at indexPath: IndexPath) -> T {
+        tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! T
+    }
+
+    static func register(in tableView: UITableView) {
+        tableView.register(UINib(nibName: identifier, bundle: nil), forCellReuseIdentifier: identifier)
+    }
 }
