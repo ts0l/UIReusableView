750223eccb1ea31cf40a50779e551044a4188654
Zach Burky <zach.burky@willowtreeapps.com>
2021-08-11 15:23:56 -0400

    Bumps floor to iOS 13.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "UIReusableView",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "UIReusableView",
